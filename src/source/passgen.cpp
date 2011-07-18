8af2cdc6c2f82525463cb12bfcef242477cc843c
Taylor Hornby <havoc@defuse.ca>
2011-07-18 22:59:00 +0000

    Added better passgen

--- src/source/passgen.cpp
+++ src/source/passgen.cpp
@@ -1,142 +1,172 @@
-//Random password generator for *nix using /dev/random
-//To make this code work with windows, modify the getRandom method to use whatever cryptographically secure RNG is included in windows.
-//Created by FireXware
-//contact: firexware@hotmail.com
-#include <iostream>
+/* Command Line Password Generator for Windows and UNIX-like systems.
+ * Copyright (C) 2011  FireXware <firexware@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Compiling with Visual C++:
+ *  cl.exe passgen.cpp advapi32.lib
+ * Compiling with G++:
+ *  g++ passgen.cpp -o passgen
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <fstream>
-using namespace std;
 
-#define random_data 128 //use 1024 random bits to create them (higher = reduces the bias when the set length isnt a multiple of the amount of random data)
-#define password_length 64 //return 64 character long passwords
+#ifdef _WIN32
+#include <windows.h>
+#include <wincrypt.h>
+#endif
+
+#define RANDOM_BYTES 80
+#define PASSWORD_LENGTH 64
 
 /*
-To visualize the bias problem, imagine this line is all of the possible combinations of the 1024 bits (0 to 2^1024)
-|------------------------------------|
-| NORMAL                         | O |
-|------------------------------------|
-
-The 'NORMAL' section is where the bar can be divided evenly by the total possible values of the password (0 to S^L where S is the character set length and L is the password length)
-The section marked 'O' is the part of a (0 to S^L) section that isnt complete, because a whole repetition will not fit.
-The longer the bar is, the shorter 'O' gets compared to the whole thing, making the chance of producing a biased password is extremely low.
-*/
-
-
-unsigned char divide(unsigned char* number, unsigned char* quotient, int len, unsigned char divisor);
-char* formatBytes(unsigned char* random512, char* set, unsigned char setlength);
-bool getRandom(unsigned char* buffer, unsigned int bufferlength);
-void showHelp();
-
-
-int main(int argc, char* argv[])
-
+ * Divides the string of bytes 'number' by 'divisor'
+ * 'quotient' is set to the result of the division with the digits reversed (for simplicity).
+ * Returns the remainder.
+ * Both number and quotient can point to the same array.
+ */
+unsigned char divide(unsigned char number[RANDOM_BYTES], unsigned char quotient[RANDOM_BYTES], unsigned char divisor)
 {
+  // Work on a copy of number so changing quotient doesn't change the number if
+  // they both point to the same array.
+  unsigned char tmp[RANDOM_BYTES];
+  memcpy(tmp, number, RANDOM_BYTES);
 
-    if(argc > 1)
-    {
-        char* set; //TODO: don't use the deprecated way of assigning the string to this
-        int setlength = 0;
-        if(strncmp(argv[1],"--help", 6) == 0)
-        {
-            showHelp();
-        }
-        else if(strncmp(argv[1], "--ascii",5) == 0)
-        {
-            set = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-            setlength = 94;
-        }
-        else if(strncmp(argv[1], "--hex", 3) == 0)
-        {
-            set = "ABCDEF0123456789";
-            setlength = 16;
-        }
-        else if(strncmp(argv[1], "--alpha", 5) == 0)
-        {
-            set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            setlength = 62;
-        }
-        else
-        {
-            showHelp();
-        }
+  unsigned char remainder = 0;
+  unsigned int total = 0;
+  for(int i = 0; i < RANDOM_BYTES; i++)
+  {
+    total = (remainder * 256 + tmp[i]);
+    quotient[RANDOM_BYTES - i - 1] = total / divisor;
+    remainder = total % divisor;
+  }
 
-        if(set != 0 && setlength > 0)
-        {
-            unsigned char* rand = new unsigned char[random_data];
-            if(getRandom(rand,random_data))
-            {
-                char* result = formatBytes(rand, set, setlength);
-                for(int i = 0; i < password_length; i++)
-                {
-                    cout << result[i];
-                }
-                cout << endl;
-                delete [] result;
-            }
-            else
-            {
-                cout << "Error reading from /dev/random." << endl;
-                return 1;
-            }
-            delete [] rand;
-        }
-    }
-    else showHelp();
-
-    return 0;
-
+  // Free secret data from memory
+  memset(tmp, 0, RANDOM_BYTES);
+  return remainder;
 }
 
-//divide - divides a byte array by a byte using long division
-//  number - the byte array containing the number to divide
-//  quotient - the result of the division, can be the same as number to store the result in the same array. must be the same length as number
-//  len - the length of 'number'
-//  divisor - the number to divide 'number' by
-//  returns - the remainder of the division
-unsigned char divide(unsigned char* number, unsigned char* quotient, int len, unsigned char divisor)
+/*
+ * Formats the string of random bytes 'randomBytes' into a password.
+ * set - A list of characters that can be in the password.
+ * setlength - the number of elements in set.
+ * result - gets filled with the password.
+ * Always creates a password of length PASSWORD_LENGTH.
+ */
+void formatBytes(unsigned char randomBytes[RANDOM_BYTES], 
+                  char* set, unsigned char setlength,
+                  unsigned char result[PASSWORD_LENGTH])
 {
-    unsigned char remainder = 0;
-    int total = 0;
-    for(int i = 0; i < len; i++)
-    {
-        total = (remainder * 256 + number[i]);
-        quotient[i] = total / divisor;
-        remainder = total % divisor;
-    }
-    return remainder;
+  for(int i = 0; i < PASSWORD_LENGTH; i++)
+  {
+    result[i] = set[divide(randomBytes, randomBytes, setlength)];
+  }
 }
 
-//formats 512 bits into a character set
-//  random512 - an array of 64 bytes
-//  set - a character array containing the character set
-//  setlength - the length of the character set <256
-//  returns - the least significant portion of random512 formatted into the character set
-char* formatBytes(unsigned char* random512, char* set, unsigned char setlength)
-{
-    char* mods = new char[password_length];
-    for(int i = 0; i < password_length; i++)
-    {
-        mods[i] = set[divide(random512, random512, random_data, setlength)];
-    }
-    //now random512 conains the data that we don't need. If we wanted to convert the entire number to the new base then we would continue the loop.
-    return mods;
-}
-
-//gets a random byte array from /dev/random
-//  buffer - a buffer to store the random data
-//  bufferlength - the length of the buffer, also the amount of random data required.
+/*
+ * Fills 'buffer' with cryptographically secure random bytes.
+ * buffer - gets filled with random bytes.
+ * bufferlength - length of buffer
+ */
 bool getRandom(unsigned char* buffer, unsigned int bufferlength)
 {
-    ifstream devRand ("/dev/random", ios::in | ios::binary);
-    return devRand.read((char*)buffer,bufferlength);
+  puts("Reading random data...");
+#ifdef _WIN32
+  HCRYPTPROV hCryptCtx = NULL;
+  CryptAcquireContext(&hCryptCtx, NULL, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
+  if(hCryptCtx == NULL)
+    return false;
+  CryptGenRandom(hCryptCtx, bufferlength, buffer);
+  CryptReleaseContext(hCryptCtx, 0);
+#else
+  FILE* random = fopen("/dev/random", "r");
+  if(random == NULL)
+    return false;
+  unsigned int read = fread(buffer, sizeof(unsigned char), bufferlength, random);
+  if(read != bufferlength)
+    return false;
+  fclose(random);
+#endif
+  return true;
 }
 
 void showHelp()
 {
-    cout << "passgen pulls 1024 bits of random data from /dev/random and converts into one of the following usable formats:" << endl
-    << "--hex 256 bit hex string" << endl
-    << "--ascii 64 character ascii printable string" << endl
-    << "--alpha 64 character alpha-numeric string" << endl
-    << "--help show this page" << endl;
+  puts("Usage: passgen <type>");
+  puts("Where <type> is one of:");
+  puts("--hex 256 bit hex string");
+  puts("--ascii 64 character ascii printable string");
+  puts("--alpha 64 character alpha-numeric string");
+  puts("--help show this page");
 }
+
+int main(int argc, char* argv[])
+{
+  if(argc != 2)
+  {
+    showHelp();
+    return 1;
+  }
+
+  char set[100]; 
+  int setlength = 0;
+  if(strncmp(argv[1],"--help", 6) == 0)
+  {
+    showHelp();
+    return 0;
+  }
+  else if(strncmp(argv[1], "--ascii",5) == 0)
+  {
+    strcpy(set, "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+    setlength = 94;
+  }
+  else if(strncmp(argv[1], "--hex", 3) == 0)
+  {
+    strcpy(set, "ABCDEF0123456789");
+    setlength = 16;
+  }
+  else if(strncmp(argv[1], "--alpha", 5) == 0)
+  {
+    strcpy(set, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+    setlength = 62;
+  }
+  else
+  {
+    showHelp();
+    return 1;
+  }
+
+  unsigned char rand[RANDOM_BYTES];
+  if(getRandom(rand, RANDOM_BYTES))
+  {
+    unsigned char result[PASSWORD_LENGTH];
+    formatBytes(rand, set, setlength, result);
+    for(int i = 0; i < PASSWORD_LENGTH; i++)
+    {
+      printf("%c", result[i]);
+    }
+    printf("\n");
+    memset(result, 0, PASSWORD_LENGTH);
+  }
+  else
+  {
+    puts("Error reading from /dev/random.");
+    return 2;
+  }
+  memset(rand, 0, RANDOM_BYTES);
+  return 0;
+}
+
