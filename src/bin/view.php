606f440324f0518ff7f90cc63a7171305b46959e
Taylor Hornby <havoc@defuse.ca>
2011-08-02 01:11:02 +0000

    pastebin utf8

--- src/bin/view.php
+++ src/bin/view.php
@@ -9,15 +9,17 @@
 if($_SERVER["HTTPS"] != "on") {
    header("HTTP/1.1 301 Moved Permanently");
    header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
-   exit();
+   die();
 }
 
 //Disable caching of viewed posts:
 header("Cache-Control: no-cache, must-revalidate"); 
 header("Expires: Mon, 01 Jan 1990 00:00:00 GMT"); 
 
+header('Content-Type: text/html; charset=utf-8');
 ?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
 <head>
 <title>Encrypted Pastebin</title>
 <style type="text/css">
