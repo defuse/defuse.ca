c95915a1472c87e20dcffcac576d49ca3c139f5a
Taylor Hornby <havoc@defuse.ca>
2011-11-26 04:47:48 +0000

    add honestyware

---
+++ src/pages/honestyware.php
@@ -0,0 +1,17 @@
+<h1>HonestyWare</h1>
+
+<h2>HonestyWare in a Nutshell</h2>
+
+<h2>Formal Definition of HonestyWare</h2>
+
+<p>HonestyWare is any computer software that meets <strong>all</strong> of the following criteria:</p>
+
+<ol>
+    <li>The full, unlimited, and official version of the software can be downloaded without charge.</li>
+    <li>The user may distribute copies of the software without charge or penalty; even if the user has not paid for the software.</li>
+    <li>The software may present an <strong>unobtrusive</strong> message to the user informing them that the software is HonestyWare only if it does not limit the software's functionality in any way.</li>
+    <li>???Once installed, the software must provide documentation explaining to the user how to share the software.</li>
+    <li>FIXUser must be able to pay any amount for the software, but author can reccomend ammounts</li>
+</ol>
+
+http://ftp.gameaholic.com/pub/games/action/quake3/models/hk-001.txt
