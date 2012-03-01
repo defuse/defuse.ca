1221548e7dc4734140d511c74fb2c0819615f54c
Taylor Hornby <havoc@defuse.ca>
2012-03-01 06:44:26 +0000

    ip shit

--- src/pages/services/ip.php
+++ src/pages/services/ip.php
@@ -7,8 +7,8 @@ HTTPS IP:
 ?>
 </div>
 
-<iframe width="100%" src="http://defuse.ca/ip-insecure.php"></iframe>
+<iframe width="100%" height="60px" src="http://defuse.ca/ip-insecure.php"></iframe>
 
 <p>
-
+This page shows your IP from two perspectives: HTTPS and HTTP. The HTTPS IP is your IP when you connect through an SSL/TLS (secure) connection. The HTTP IP is your IP when you connect through a normal unencrypted connection. Normally the IP addresses will be the same. If they are different, it means there is a server/router between you and this website that is automatically and transparently caching your HTTP traffic.
 </p>
