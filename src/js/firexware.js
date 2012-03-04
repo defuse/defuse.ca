e32402a98dd83fd472d489e991e0f732c9e1c3ee
Taylor Hornby <havoc@defuse.ca>
2012-03-04 18:17:54 +0000

    pb js

--- src/js/firexware.js
+++ src/js/firexware.js
@@ -72,8 +72,8 @@ fxw.allhtmlsani = function(text)
 	//Now deal with spaces, tabs, and newlines
 	text = text.replace("/\r\n/g", "\n");
 	text = text.replace("/\r/g", "\n");
-	text = text.replace("/\ \ /g", "&nbsp;&nbsp;");
 	text = text.replace("/\t/g", "&nbsp;&nbsp;&nbsp;&nbsp;");
+	text = text.replace("/\s\s/g", "&nbsp;&nbsp;");
 	text = text.replace("/\n/g", "<br />");
 	return text;
 }
