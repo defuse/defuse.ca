536fc7f1cd8529eae763ab2ff0201e2b63be5f65
Taylor Hornby <havoc@defuse.ca>
2012-03-18 02:38:13 +0000

    added sample output to php hash cracker

--- src/pages/projects/php-hash-cracker.php
+++ src/pages/projects/php-hash-cracker.php
@@ -4,6 +4,31 @@
 The following is a PHP script for running dictionary attacks against both salted and unsalted password hashes. It is capable of cracking every hash function supported by PHP's <a href="http://php.net/hash">hash</a> function, as well as LM, NTLM, MySQL 4.1, and crypt hashes. It also supports crashed session recovery.
 </p>
 
+<h2>Sample Output</h2>
+<div class="code">
+$ php crack.php -w small.lst -c 2c5419e6db59f283bcbb501c722e73c6 -t md5 -l a8f0h2 -r 8hf27<br />
+Defuse Cyber-Security&#039;s Hash Cracking Script - v1.0<br />
+Homepage: https://defuse.ca/php-hash-cracker.htm<br />
+<br />
+Begin execution: March 17, 2012, 8:31 pm <br />
+Wordlist: small.lst <br />
+Start line: 0 <br />
+Hash: 2c5419e6db59f283bcbb501c722e73c6 <br />
+Hash type: md5 <br />
+Left salt: a8f0h2 <br />
+Right salt: 8hf27 <br />
+<br />
+Current Line: 1000000&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Current Password: IndigoIndigo &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <br />
+Current Line: 2000000&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Current Password: 5reinforce &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Rate: 239 k/s<br />
+PASSWORD FOUND: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Zygomaticing (0x5a79676f6d61746963696e67)<br />
+HASH:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2c5419e6db59f283bcbb501c722e73c6<br />
+HASH TYPE:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;md5<br />
+LEFT SALT: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a8f0h2 (0x613866306832)<br />
+RIGHT SALT: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8hf27 (0x3868663237)<br />
+</div>
+
+<h2>Code</h2>
+
 <div class="code">
 &lt;?php<br />
 /*<br />
