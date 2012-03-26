427ccccdfdd464d3844b250e597431d410b26f80
Taylor Hornby <havoc@defuse.ca>
2012-03-26 23:02:26 +0000

    added code to html sanitize

--- src/pages/services/html-sanitize.php
+++ src/pages/services/html-sanitize.php
@@ -43,3 +43,18 @@ if(isset($_POST['data']))
     <input type="checkbox" name="br" value="yes" checked="checked" />Replace line endings with &lt;br /&gt;
 </form>
 
+<h2>How it works</h2>
+
+<p>Escaping text is a bit tricky when you want it to look exactly the same in HTML as it would look in a text editor.
+ The code this page uses to escape text is presented below. If you read the comments, you'll see that the solution is
+ not as obvious as you might think!
+</p>
+
+<div class="code">
+    <?php
+        $file = file_get_contents('libs/HtmlEscape.php');
+        $file_esc = HtmlEscape::escapeText($file, true, 4);
+        echo $file_esc;
+    ?>
+</div>
+
