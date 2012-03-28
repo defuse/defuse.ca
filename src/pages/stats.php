8d050c69120610b22f440c223e5c67000217cefe
Taylor Hornby <havoc@defuse.ca>
2012-03-28 08:12:53 +0000

    really added stats

---
+++ src/pages/stats.php
@@ -0,0 +1,37 @@
+<h1>Server Statistics</h1>
+
+<h2>uptime</h2>
+<div class="code">
+<pre>
+<?php
+    passthru("uptime");
+?>
+</pre>
+</div>
+
+<h2>ifconfig</h2>
+<div class="code">
+<pre>
+<?php
+    passthru("ifconfig");
+?>
+</pre>
+</div>
+
+<h2>who</h2>
+<div class="code">
+<pre>
+<?php
+    passthru("who");
+?>
+</pre>
+</div>
+
+<h2>netstat</h2>
+<div class="code">
+<pre>
+<?php
+    passthru("netstat -tn");
+?>
+</pre>
+</div>
