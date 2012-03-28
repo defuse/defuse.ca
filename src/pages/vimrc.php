5a96366f6619b517125155422ed76af055c9eb44
Taylor Hornby <havoc@defuse.ca>
2012-03-28 05:38:28 +0000

    vimrc header prgph

--- src/pages/vimrc.php
+++ src/pages/vimrc.php
@@ -1,5 +1,11 @@
 <h1>My .vimrc</h1>
 
+<p>
+    This page is updated with the .vimrc on my PC at home every midnight.
+    The color scheme I use is
+    <a href="https://vimcolorschemetest.googlecode.com/svn/colors/dw_cyan.vim">dw_cyan</a>.
+</p>
+
 <div class="code">
     <?php
         require_once('libs/HtmlEscape.php');
