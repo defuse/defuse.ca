7b938280dd88daa1c018753a0fe7297bf3df969d
Taylor Hornby <havoc@defuse.ca>
2012-03-28 08:21:45 +0000

    added password to stats page

--- src/pages/stats.php
+++ src/pages/stats.php
@@ -1,37 +1,55 @@
 <h1>Server Statistics</h1>
 
-<h2>uptime</h2>
-<div class="code">
-<pre>
 <?php
-    passthru("uptime");
-?>
-</pre>
-</div>
 
-<h2>ifconfig</h2>
-<div class="code">
-<pre>
-<?php
-    passthru("ifconfig");
+if(isset($_POST['p']) && md5("fh8f8hf" . $_POST['p']) == "7da89b20da373c455a31fc33301d2d10")
+{
 ?>
-</pre>
-</div>
+    <h2>uptime</h2>
+    <div class="code">
+    <pre>
+    <?php
+        passthru("uptime");
+    ?>
+    </pre>
+    </div>
 
-<h2>who</h2>
-<div class="code">
-<pre>
-<?php
-    passthru("who");
-?>
-</pre>
-</div>
+    <h2>ifconfig</h2>
+    <div class="code">
+    <pre>
+    <?php
+        passthru("ifconfig");
+    ?>
+    </pre>
+    </div>
 
-<h2>netstat</h2>
-<div class="code">
-<pre>
-<?php
-    passthru("netstat -tn");
+    <h2>who</h2>
+    <div class="code">
+    <pre>
+    <?php
+        passthru("who");
+    ?>
+    </pre>
+    </div>
+
+    <h2>netstat</h2>
+    <div class="code">
+    <pre>
+    <?php
+        passthru("netstat -tn");
+    ?>
+    </pre>
+    </div>
+<?
+}
+else
+{
 ?>
-</pre>
-</div>
+    <form action="/stats.htm" method="post">
+        <input type="password" name="p" value="">
+        <input type="submit" name="" value="Go" />
+    </form>
+<?
+}
+
+?>
