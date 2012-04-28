ec73092c5b10a42fa06170c8f15cd5eb0ca75e18
Taylor Hornby <havoc@defuse.ca>
2012-04-28 20:39:42 +0000

    added download button to pbkdf2

--- src/pages/projects/php-pbkdf2.php
+++ src/pages/projects/php-pbkdf2.php
@@ -1,6 +1,10 @@
 <h1>PBKDF2 For PHP</h1>
 <p>PBKDF2 (Password Based Key Derivation Function) for PHP. The following code is public domain, feel free to use it for any purpose. The code complies with test vectors at <a href="https://www.ietf.org/rfc/rfc6070.txt">https://www.ietf.org/rfc/rfc6070.txt</a>.</p>
 
+<div style="text-align: center; padding: 20px;">
+    <a href="/source/pbkdf2.php"><img src="/images/download.png" alt="Download" /></a>
+</div>
+
 <div class="code" >
     <?php
         require_once('libs/HtmlEscape.php');
