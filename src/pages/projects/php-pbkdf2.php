3bb4f5b5f3448ff3bcccaee3e1c31e955140b243
Taylor Hornby <havoc@defuse.ca>
2012-06-24 18:39:13 +0000

    added pbkdf2 benchmark and improved introduction

--- src/pages/projects/php-pbkdf2.php
+++ src/pages/projects/php-pbkdf2.php
@@ -1,8 +1,55 @@
 <h1>PBKDF2 For PHP</h1>
-<p>PBKDF2 (Password Based Key Derivation Function) for PHP. The following code is public domain. Feel free to use it for any purpose. The code complies with test vectors at <a href="https://www.ietf.org/rfc/rfc6070.txt">https://www.ietf.org/rfc/rfc6070.txt</a>. Performance improvements were provided by <a href="http://www.variations-of-shadow.com">http://www.variations-of-shadow.com</a>.</p>
 
-<div style="text-align: center; padding: 20px;">
-    <a href="/source/pbkdf2.php"><img src="/images/download.png" alt="Download" /></a>
+<p>
+PBKDF2 (Password-Based Key Derivation Function) is a <a href="http://en.wikipedia.org/wiki/Key_stretching">key stretching</a> algorithm.
+It can be used to hash passwords in a computationally intensive manner, so that dictionary and
+brute-force attacks are less effective.</p>
+
+<p>
+The following code is a PBKDF2 implementation in PHP. It is in the public domain, so feel free to use
+it for any purpose whatsoever. It complies with the <a href="https://www.ietf.org/rfc/rfc6070.txt">PBKDF2 test vectors in RFC 6070</a>. Performance improvements to the original code were provided by <a href="http://www.variations-of-shadow.com/">variations-of-shadow.com</a>.
+</p>
+
+
+<h2>Benchmarks</h2>
+
+<p>
+The following benchmarks demonstrate the running time for various iteration counts. The benchmarks
+were run on an AMD Phenom 9600 2.3GHz CPU.
+</p>
+
+<div class="code">
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 1 iteration : 0.000149 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 2 iterations: 0.000036 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 4 iterations: 0.000038 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; 8 iterations: 0.000052 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;16 iterations: 0.000090 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;32 iterations: 0.000157 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;64 iterations: 0.000297 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; 128 iterations: 0.000623 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; 256 iterations: 0.001141 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp; 512 iterations: 0.002258 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp;1024 iterations: 0.004594 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp;2048 iterations: 0.009575 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp;4096 iterations: 0.018386 seconds<br />
+&nbsp;&nbsp; &nbsp; &nbsp;8192 iterations: 0.036070 seconds<br />
+&nbsp;&nbsp; &nbsp; 16384 iterations: 0.073297 seconds<br />
+&nbsp;&nbsp; &nbsp; 32768 iterations: 0.145324 seconds<br />
+&nbsp;&nbsp; &nbsp; 65536 iterations: 0.294785 seconds<br />
+&nbsp;&nbsp; &nbsp;131072 iterations: 0.577492 seconds<br />
+&nbsp;&nbsp; &nbsp;262144 iterations: 1.173854 seconds<br />
+&nbsp;&nbsp; &nbsp;524288 iterations: 2.334627 seconds<br />
+&nbsp;&nbsp; 1048576 iterations: 4.688382 seconds<br />
+&nbsp;&nbsp; 2097152 iterations: 9.249891 seconds<br />
+&nbsp;&nbsp; 4194304 iterations: 18.69492 seconds<br />
+&nbsp;&nbsp; 8388608 iterations: 36.90171 seconds<br />
+&nbsp;&nbsp;16777216 iterations: 75.31797 seconds
+</div>
+
+<h2>Source Code</h2>
+
+<div style="text-align: center; padding-bottom: 10px;">
+    <a href="/source/pbkdf2.php"><strong>Click Here to Download</strong></a>
 </div>
 
 <div class="code" >
