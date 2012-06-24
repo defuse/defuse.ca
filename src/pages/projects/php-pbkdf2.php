273b30dcc05e22f2c50eeeead3dc984588a5b9fd
Taylor Hornby <havoc@defuse.ca>
2012-06-24 18:00:48 +0000

    improvements to pbkdf2 code

--- src/pages/projects/php-pbkdf2.php
+++ src/pages/projects/php-pbkdf2.php
@@ -1,5 +1,5 @@
 <h1>PBKDF2 For PHP</h1>
-<p>PBKDF2 (Password Based Key Derivation Function) for PHP. The following code is public domain, feel free to use it for any purpose. The code complies with test vectors at <a href="https://www.ietf.org/rfc/rfc6070.txt">https://www.ietf.org/rfc/rfc6070.txt</a>.</p>
+<p>PBKDF2 (Password Based Key Derivation Function) for PHP. The following code is public domain. Feel free to use it for any purpose. The code complies with test vectors at <a href="https://www.ietf.org/rfc/rfc6070.txt">https://www.ietf.org/rfc/rfc6070.txt</a>. Performance improvements were provided by <a href="http://www.variations-of-shadow.com">http://www.variations-of-shadow.com</a>.</p>
 
 <div style="text-align: center; padding: 20px;">
     <a href="/source/pbkdf2.php"><img src="/images/download.png" alt="Download" /></a>
--- src/source/pbkdf2.php
+++ src/source/pbkdf2.php
@@ -7,11 +7,15 @@
      * $salt - A salt that is unique to the password.
      * $count - Iteration count. Higher is better, but slower. Recommended: At least 1024.
      * $key_length - The length of the derived key in BYTES.
-     * Returns: A $key_length-byte key derived from the password and salt (in binary).
+     * $hex - binary or hex output. default: false => binary
+     * Returns: A $key_length-byte key derived from the password and salt.
      *
      * Test vectors can be found here: https://www.ietf.org/rfc/rfc6070.txt
+     *
+     * This implementation of PBKDF2 was originally created by https://defuse.ca
+     * With improvements by http://www.variations-of-shadow.com
      */
-    function pbkdf2($algorithm, $password, $salt, $count, $key_length)
+    function pbkdf2($algorithm, $password, $salt, $count, $key_length, $hex = false)
     {
         $algorithm = strtolower($algorithm);
         if(!in_array($algorithm, hash_algos(), true))
@@ -26,38 +30,22 @@
             $block_count += 1;
 
         $output = "";
-        for($i = 1; $i <= $block_count; $i++)
-        {
-            $output .= pbkdf2_f($password, $salt, $count, $i, $algorithm, $hash_length);
-        }
-
-        return substr($output, 0, $key_length);
-    }
-
-    /*
-     * The pseudorandom function used by PBKDF2.
-     * Definition: https://www.ietf.org/rfc/rfc2898.txt
-     */
-    function pbkdf2_f($password, $salt, $count, $i, $algorithm, $hash_length)
-    {
-        //$i encoded as 4 bytes, big endian.
-        $last = $salt . chr(($i >> 24) % 256) . chr(($i >> 16) % 256) . chr(($i >> 8) % 256) . chr($i % 256);
-        $xorsum = "";
-        for($r = 0; $r < $count; $r++)
-        {
-            $u = hash_hmac($algorithm, $last, $password, true);
-            $last = $u;
-            if(empty($xorsum))
-                $xorsum = $u;
-            else
-            {
-                for($c = 0; $c < $hash_length; $c++)
-                {
-                    $xorsum[$c] = chr(ord(substr($xorsum, $c, 1)) ^ ord(substr($u, $c, 1)));
-                }
+        for($i = 1; $i <= $block_count; $i++) {
+            //$i encoded as 4 bytes, big endian.
+            $last = $salt . pack("N", $i);
+            // first iteration
+            $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
+            // perform the other $count - 1 iterations
+            for ($j = 1; $j < $count; $j++) {
+                $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
             }
+            $output .= $xorsum;
         }
-        return $xorsum;
+
+        if($hex)
+            return bin2hex(substr($output, 0, $key_length));
+        else
+            return substr($output, 0, $key_length);
     }
 
 ?>
