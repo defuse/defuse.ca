44869d1b7193d3760695bf82f623399f683ccda7
Taylor Hornby <havoc@defuse.ca>
2012-08-20 03:59:54 +0000

    push out the utilities menu because it is too many levels of indirection

--- src/index.php
+++ src/index.php
@@ -114,14 +114,10 @@ header('Content-Type: text/html; charset=utf-8');
             <li><a href="/trustedthirdparty.htm">&nbsp;TRENT - Trusted RNG</a></li>
             <li><a href="/pdfcleaner.htm">&nbsp;PDFCleaner</a></li>
 
-            <li><a href="#"><span class="drop"><span>Utilities</span>&raquo;</span><!--[if gt IE 6]><!--></a><!--<![endif]--><!--[if lt IE 7]><table border="0" cellpadding="0" cellspacing="0"><tr><td><![endif]-->
-                <ul>
-                    <li><a href="/big-number-calculator.htm">&nbsp;Big Number Calculator</a></li>
-                    <li><a href="/checksums.htm">&nbsp;Checksum Calculator</a></li>
-                    <li><a href="/html-sanitize.htm">&nbsp;HTML Sanitizer</a></li>
-                </ul>
-                <!--[if lte IE 6]></td></tr></table></a><![endif]-->
-            </li>
+            <li><a href="/big-number-calculator.htm">&nbsp;Big Number Calculator</a></li>
+            <li><a href="/checksums.htm">&nbsp;Checksum Calculator</a></li>
+            <li><a href="/html-sanitize.htm">&nbsp;HTML Sanitizer</a></li>
+
             <li><a href="http://crackstation.net/">&nbsp;CrackStation.net</a></li>
 
         </ul>
