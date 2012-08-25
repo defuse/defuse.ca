893873c30b57c11a64ba6c52852313f12d22cf6f
Taylor Hornby <havoc@defuse.ca>
2012-08-25 18:42:11 +0000

    added screenshot of command line vim

--- src/pages/vimrc.php
+++ src/pages/vimrc.php
@@ -3,18 +3,20 @@
 <p>
     This page is updated with the .vimrc on my PC at home every midnight.
     The file can be downloaded without formatting <a href="/source/vimrc">here</a>.
-    The color scheme I use is
-    <a href="https://vimcolorschemetest.googlecode.com/svn/colors/dw_cyan.vim">dw_cyan</a>.
+    The color scheme I use for gVim is
+    <a href="https://vimcolorschemetest.googlecode.com/svn/colors/dw_cyan.vim">dw_cyan</a>, and I
+    use <a href="https://github.com/nanotech/jellybeans.vim">jellybeans</a> for command-line Vim.
 </p>
 
 <p>
     Download my entire .vim directory <a href="/source/vim.zip">here</a>.
 </p>
 
-<h2>Screenshot</h2>
-
+<h2>Screenshots</h2>
+<p style="text-align: center;"><i>These screenshots are not updated automatically, and may be out of date.</i></p>
 <center>
-<img src="/images/vim.png" alt="My Vim configuration"/>
+<img src="/images/vim.png" alt="My Vim configuration (gVim)"/> <br /><br />
+<img src="/images/vim-cli.png" alt="My Vim configuration (command-line Vim)"/>
 </center>
 
 <h2>Synchronization Script</h2>
