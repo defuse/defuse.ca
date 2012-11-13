20815be31ae88459b5af06dde00ad1a78efd8efb
Taylor Hornby <havoc@defuse.ca>
2012-11-13 06:50:48 +0000

    - make defuse 404 page even better

--- src/pages/404.php
+++ src/pages/404.php
@@ -1,6 +1,6 @@
 <div style="text-align:center;">
 <span style="font-size: 36pt;">Oops!</span><br />
-<span style="font-size: 26pt;">This page doesn't exist!</span> <br /><br /><br /><br />
+<span style="font-size: 26pt;">This page does not exist.</span> <br /><br /><br /><br />
 <img src="/images/divzero.png" /> <br /><br /><br /><br />
 <span style="font-size: 14pt; color: #BBBBBB;">(ERROR 404)</span>
 </div>
