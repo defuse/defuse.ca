6767cdcceb336196b09a0e078fb957bc6dab584a
Taylor Hornby <havoc@defuse.ca>
2012-11-13 08:18:54 +0000

    - imrpove 404 page again

--- src/pages/404.php
+++ src/pages/404.php
@@ -2,5 +2,5 @@
 <span style="font-size: 36pt;">Oops!</span><br />
 <span style="font-size: 26pt;">This page does not exist.</span> <br /><br /><br /><br />
 <img src="/images/divzero.png" /> <br /><br /><br /><br />
-<span style="font-size: 14pt; color: #BBBBBB;">(ERROR 404)</span>
+<span style="font-size: 14pt; color: #888888;">(ERROR 404)</span>
 </div>
