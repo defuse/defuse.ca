92b8cde08e97f763d562581f04799cc9910422d9
Taylor Hornby <havoc@defuse.ca>
2012-12-15 01:02:59 +0000

    - add disclaimer to key stretching feasi page

--- src/pages/research/website-key-stretching-feasibility.php
+++ src/pages/research/website-key-stretching-feasibility.php
@@ -1,5 +1,21 @@
 <h1>Feasibility of Using Key Stretching in Web Applications</h4>
 
+<p>
+    <b>IMPORTANT NOTE:</b> The data presented here is not of scientific quality. I created this page
+in the preliminary stages of some research into whether or not key stretching is as beneficial
+for web applications as it is for stand-alone apps running on the user's PC.
+</p>
+
+<p>
+    The testing was done on Debian 6 + Apache, serving a simple PHP script that loads one (the same)
+hash out of MySQL, computes the PBKDF2 password from $_GET, and compares the hash. Obviously this is
+not a realistic scenario.
+</p>
+
+<p>
+So please do not take these graphs too seriously!
+</p>
+
 <center>
     <img src="images/siege/p4box-availability.png" /><br />
     <img src="images/siege/p4box-concurrency.png" /><br />
