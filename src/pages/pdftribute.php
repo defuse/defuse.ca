df74ec1ee8e7310d3d6c26f9740c4e589ba33213
Taylor Hornby <havoc@defuse.ca>
2013-01-15 05:26:40 +0000

    - better pdftribute-big

--- src/pages/pdftribute.php
+++ src/pages/pdftribute.php
@@ -1,7 +1,11 @@
 <h1>#pdftribute</h1>
 
 <center>
+    Small:<br />
     <a href="/source/pdftribute.tex"><strong>Download pdftribute.tex</strong></a> <br />
     <a href="/downloads/pdftribute.pdf"><strong>Download pdftribute.pdf</strong></a> <br /> <br />
+    Big:<br />
+    <a href="/source/pdftribute-big.tex"><strong>Download pdftribute-big.tex</strong></a> <br />
+    <a href="/downloads/pdftribute-big.pdf"><strong>Download pdftribute-big.pdf</strong></a> <br /> <br />
     <img src="/images/pdftribute-poster.jpg" alt="#pdftribute posters cut up" style="border: solid black 2px;" />
 </center>
