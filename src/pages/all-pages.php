f1c09d8a27f11e63705275a8d312e7e7f7244b3a
Taylor Hornby <havoc@defuse.ca>
2013-04-15 05:12:44 +0000

    - add disclaimer to all-pages page

--- src/pages/all-pages.php
+++ src/pages/all-pages.php
@@ -1,4 +1,8 @@
 <h1>All Pages (By Popularity)</h1>
+<p>
+Note: The upvote system is still in beta, and I haven't had a chance to add it to all of our pages.
+Please upvote the pages you like and downvote the pages you don't.
+</p>
 <?php
     Upvote::render_list();
 ?>
