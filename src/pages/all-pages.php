843282206a3dd3d52c9e4f17c0790825e24066d6
Taylor Hornby <havoc@defuse.ca>
2013-05-24 20:10:36 -0600

    Remove beta status of upvote pages.

--- src/pages/all-pages.php
+++ src/pages/all-pages.php
@@ -1,7 +1,7 @@
 <h1>All Pages (By Popularity)</h1>
 <p>
-Note: The upvote system is still in beta, and I haven't had a chance to add it to all of our pages.
-Please upvote the pages you like and downvote the pages you don't.
+This is a list of all pages on this web site, sorted on the amount of upvotes and downvotes visitors
+have given them. Please upvote the pages you like and downvote the ones you don't.
 </p>
 <?php
     Upvote::render_list();
