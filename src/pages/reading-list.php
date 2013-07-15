8af7f83ce54c7e67b277ed9c88c927097453b2bc
Taylor Hornby <havoc@defuse.ca>
2013-07-15 13:28:18 -0600

    Add a paper to reading list.

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -97,6 +97,7 @@ A web search of the title is usually enough to find the book or paper.
 <h3>2013</h3>
 
 <ul>
+  <li>PLTO: A Link-Time Optimizer for the Intel IA-32 Architecture</li>
   <li><b>Impeding Malware Analysis Using Conditional Code Obfuscation (Sharif et al.)</b></li>
   <li><b>Obfuscation of Executable Code to Improve Resistance to Static Disassembly (Cullen Linn, Saumya Debray)</b></li>
   <li>Dissecting Android Malware: Characterization and Evolution (Yajin Zhou, Xuxian Jiang)</li>
