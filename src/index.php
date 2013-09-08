ceb3902eca46320037cd988659b462c0381cdf86
Taylor Hornby <havoc@defuse.ca>
2013-09-07 21:57:53 -0600

    Add jquery and js_string_escape for use by pages.

--- src/index.php
+++ src/index.php
@@ -40,6 +40,19 @@ function printSourceFile($path, $numbers = false) {
     echo '<div class="vimhighlight">' . $hl->processFile($path, true) . '</div>';
 }
 
+function js_string_escape($data)
+{
+    $safe = "";
+    for($i = 0; $i < strlen($data); $i++)
+    {
+        if(ctype_alnum($data[$i]))
+            $safe .= $data[$i];
+        else
+            $safe .= sprintf("\\x%02X", ord($data[$i]));
+    }
+    return $safe;
+} 
+
 function action_alert()
 {
     return;
@@ -103,6 +116,7 @@ header('Content-Type: text/html; charset=utf-8');
 <meta name="google-site-verification" content="LjgndE9fyTkxbPz8aMFyJQFSS3cQiXIrYchE_b2VXlg" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="text/javascript" src="/js/upvote.js"></script>
+<script type="text/javascript" src="/js/jquery.js"></script>
 <link rel="stylesheet" media="all" type="text/css" href="/main.css" />
 <link rel="stylesheet" media="all" type="text/css" href="/mainmenu.css" />
 <link rel="stylesheet" media="all" type="text/css" href="/vimhl.css" />
