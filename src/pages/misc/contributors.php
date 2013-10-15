9f909ac720b19bc48a174a377d5599ec7f6f1749
Taylor Hornby <havoc@defuse.ca>
2013-10-15 10:55:07 -0600

    Add the actual contributors page.

---
+++ src/pages/misc/contributors.php
@@ -0,0 +1,13 @@
+<h1>Contributors</h1>
+
+<p>
+The people and organizations listed on this page have made important voluntary
+contributions to our work. Without them, some of our projects would not have
+been possible.
+</p>
+
+<ul>
+    <li>
+        <a href="https://dicesoft.net/">Stephen Chavez</a>
+    </li>
+</ul>
