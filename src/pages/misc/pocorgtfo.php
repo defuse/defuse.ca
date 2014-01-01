060e7deb027ab06ede026d2a4d375e06d9a3e5c2
Taylor Hornby <havoc@defuse.ca>
2014-01-01 14:55:54 -0700

    Add the actual pocorgtfo page.

---
+++ src/pages/misc/pocorgtfo.php
@@ -0,0 +1,11 @@
+<h1>PoC || GTFO Mirror</h1>
+
+<p>
+This mirror is only a proof of concept. It may not be up to date.
+</p>
+
+<ul>
+    <li><a href="/files2/poc/pocorgtfo00.pdf">Issue 0x00</a></li>
+    <li><a href="/files2/poc/pocorgtfo01.pdf">Issue 0x01</a></li>
+    <li><a href="/files2/poc/pocorgtfo02.pdf">Issue 0x02</a></li>
+</ul>
