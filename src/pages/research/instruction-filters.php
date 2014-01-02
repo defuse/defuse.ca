a2b4d1cf76a5954e2774bc56985f241b8ed64cb1
Taylor Hornby <havoc@defuse.ca>
2014-01-02 07:01:02 -0700

    Link to bochs page from research page.

--- src/pages/research/instruction-filters.php
+++ src/pages/research/instruction-filters.php
@@ -7,5 +7,7 @@ a research project to determine whether disabling CPU instructions based on the
 current execution context could serve as an effective exploit defense mechanism.
 The research is not complete (I'm trying to find time to finish it!), but you
 can find the slides from a presentation I gave <a
-href="https://defuse.ca/files2/insfilter.pdf">here</a>.
+href="https://defuse.ca/files2/insfilter.pdf">here</a>. The <a
+href="/bochs-hacking-guide.htm">Bochs Hacking Guide</a> is also a result of this
+research.
 </p>
