377188c0a335c83a4e6b6b4facb6e504b929a61c
Taylor Hornby <havoc@defuse.ca>
2014-01-22 14:38:44 -0700

    Improve software security auditing page and link it from homepage.

--- src/pages/services/software-security-auditing.php
+++ src/pages/services/software-security-auditing.php
@@ -1,21 +1,21 @@
-<h1>Is your software secure?</h1>
+<h1>Software Security Auditing</h1>
 
 <p>
-I want to work with you to make sure your code is secure. So I've decided to
-start a part-time security auditing service. I'm available 10 hours per week, for
-$20 CAD per hour, to look over your designs and code to make sure they are
-secure. To give back to the community, I'll reduce my rate to $15 per hour for
-published free software (under a GPL-like license).
+I want to work with you to make sure your code is secure. I'm available 10 hours
+per week, for $20 CAD per hour, to look over your designs and code to make sure
+they are secure. To give back to the community, I'll reduce my rate to $15 per
+hour for published free or open source software (GPL-like or BSD-like license).
 </p>
 
 <p>
 I am not an experienced code auditor, but it's something I <a
 href="http://www.cryptofails.com/">really like to do</a>,
 and I am pretty good with security (see my <a
-href="/resume.htm">r&eacute;sum&eacute;</a>). So this will also be a learning experience for
-me. That's good for you, too: I will not charge for the time I need to spend
-learning new technologies in order to evaulate your security. I'll only charge
-for the time I'm actually looking at your design/code and writing reports.
+href="/resume.htm">r&eacute;sum&eacute;</a>). So this will also be a learning
+experience for me. That's good for you, too: I will not charge for the time
+I need to spend learning new technologies in order to evaluate your security.
+I'll only charge for the time I'm actually looking at your design/code and
+writing reports.
 </p>
 
 <p>
@@ -23,27 +23,30 @@ What I can do:
 </p>
 
 <ul>
-    <li>Check web code for XSS, SQL injection, session, password hashing, etc.  vulnerabilities.</li>
-    <li>Evaulate the design and implementation of cryptography software.</li>
+    <li>Check web code for XSS, SQL injection, session, password hashing, etc. vulnerabilities.</li>
+    <li>Evaluate the design and implementation of cryptography software.</li>
     <li>Look for memory corruption bugs in C and C++ code.</li>
     <li>Write proof-of-concept exploits for vulnerabilities.</li>
-    <li>Check the security of your Linux server and the services it runs.</li>
+    <li>Check the configuration of your Linux server.</li>
     <li>Responsibly disclose vulnerabilities to vendors.</li>
 </ul>
 
 <p>
-Here's an <a href="/audits/encfs.htm">example of my work</a>. I am especially
-interested in auditing software that uses cryptography.
+Here are some examples of my work:
+</p>
+
+<ul>
+    <li><a href="/audits/encfs.htm">EncFS</a></li>
+    <li><a href="/audits/ecryptfs.htm">eCryptfs</a></li>
+</ul>
+
+<p>
+To make the most effective use of my time, I will work with you to prioritize
+which parts of your application should be audited first. When the audit is
+complete, I will work with you to ensure the problems are fixed correctly.
 </p>
 
 <p>
-To make the most effective use of my limited time, I will work with you to
-prioritize which parts of your application should be audited first. When the
-audit is complete, I will work with you to ensure the problems are fixed
-correctly.
-</p>
-
-<p>
-Interested? Email <a href="mailto:havoc@defuse.ca">havoc@defuse.ca</a> or
+Interested? Email me at <a href="mailto:havoc@defuse.ca">havoc@defuse.ca</a> or
 mention <a href="https://twitter.com/defusesec">@DefuseSec</a> on twitter.
 </p>
