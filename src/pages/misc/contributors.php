bc5299f62f848fbf8fe7bebcc907e315400c97db
Taylor Hornby <havoc@defuse.ca>
2014-02-10 13:41:30 -0700

    Add solar designer to contributors page.

--- src/pages/misc/contributors.php
+++ src/pages/misc/contributors.php
@@ -1,13 +1,23 @@
 <h1>Contributors</h1>
 
 <p>
-The people and organizations listed on this page have made important voluntary
-contributions to our work. Without them, some of our projects would not have
-been possible.
+We thank the following people for helping our work. <b>The people on this list
+do not necessarily endorse/support this website.</b> We just want to say thanks
+to:
 </p>
 
 <ul>
     <li>
-        <a href="https://dicesoft.net/">Stephen Chavez</a>
+        <a href="https://dicesoft.net/">Stephen Chavez</a> for:
+        <ul>
+            <li>Ruby password hashing code.</li>
+        </ul>
+    </li>
+    <li>
+        <a href=http://openwall.info/wiki/people/solar/bio">Solar Designer</a> for:
+        <ul>
+            <li>Very useful criticism of my password hashing article.</li>
+            <li>Advice on various things (side channel resistence, etc.)</li>
+        </ul>
     </li>
 </ul>
