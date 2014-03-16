def9beda457d7dc120f6af2440398f8fb08c8409
Taylor Hornby <havoc@defuse.ca>
2014-03-16 09:47:29 -0600

    Add poc||gtfo 0x03 to mirror

--- src/pages/misc/pocorgtfo.php
+++ src/pages/misc/pocorgtfo.php
@@ -8,4 +8,5 @@ This mirror is only a proof of concept. It may not be up to date.
     <li><a href="/files2/poc/pocorgtfo00.pdf">Issue 0x00</a></li>
     <li><a href="/files2/poc/pocorgtfo01.pdf">Issue 0x01</a></li>
     <li><a href="/files2/poc/pocorgtfo02.pdf">Issue 0x02</a></li>
+    <li><a href="/files2/poc/pocorgtfo03.pdf">Issue 0x03</a></li>
 </ul>
