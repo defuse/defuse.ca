5668ee116417156ea7194f54d55db55e3312b9be
Taylor Hornby <havoc@defuse.ca>
2014-05-30 22:51:52 -0600

    Add exploit regulation paper to reading list

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -149,6 +149,7 @@ A web search of the title is usually enough to find the book or paper.
 <h3>2014</h3>
 
 <ul>
+    <li>(DRAFT) Avoiding a War on Unauthorized Computation: Why Exploit Regulation is the Biggest Danger to Coder Freedom and Future Security (Sergey Bratus, Anna Shubina)</li>
     <li>Leibniz's Mill Argument Against Mechanical Materialism Revisted (Paul Lodge)</li>
     <li>Using Clustering to Detect Chinese Censorware (Becker Polverini, William M. Pottenger)</li>
     <li><b>The Collateral Damage of Internet Censorship by DNS Injection (Five Anonymous Authors)</b></li>
