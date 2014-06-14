5bc59fb05da14ec9b679de00c2cd47373eb68b48
Taylor Hornby <havoc@defuse.ca>
2014-06-14 08:48:45 -0600

    Move blog into /blog

--- blog-source/blogsettings.py
+++ blog-source/blogsettings.py
@@ -1,11 +1,11 @@
 PATH = 'content'
 AUTHOR = u'Taylor Hornby'
 SITENAME = u'The Code Files'
-SITEURL = '/blog2'
+SITEURL = '/blog'
 
 SOCIAL = (('twitter', 'https://twitter.com/DefuseSec'),
           ('github', 'https://github.com/defuse'),)
 
 TIMEZONE = "America/Edmonton"
 DELETE_OUTPUT_DIRECTORY = True
-OUTPUT_PATH = '../src/blog2/'
+OUTPUT_PATH = '../src/blog/'
