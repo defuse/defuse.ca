8bfb4a65dcd44e1c3b9861ba3d1ade94369ea4ea
Taylor Hornby <havoc@defuse.ca>
2014-08-10 12:04:31 -0600

    Add PoC||GTFO 0x05

--- src/pages/misc/pocorgtfo.php
+++ src/pages/misc/pocorgtfo.php
@@ -10,4 +10,5 @@ This mirror is only a proof of concept. It may not be up to date.
     <li><a href="/files2/poc/pocorgtfo02.pdf">Issue 0x02</a></li>
     <li><a href="/files2/poc/pocorgtfo03.pdf">Issue 0x03</a></li>
     <li><a href="/files2/poc/pocorgtfo04.pdf">Issue 0x04</a></li>
+    <li><a href="/files2/poc/pocorgtfo05.pdf">Issue 0x05</a></li>
 </ul>
