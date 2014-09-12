8ab9a2df6a97e18de4ee20dd7b274828f1eab980
Taylor Hornby <havoc@defuse.ca>
2014-09-11 19:06:43 -0600

    Add shitty heurstic paper to RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -124,6 +124,7 @@ search of the title is usually enough to find the book or paper.
 <h3>2014</h3>
 
 <ul>
+    <li>Beyond Heuristics: Learning to Classify Vulnerabilities and Predict Exploits (Mehran Bozorgi et al.)</li>
     <li><b>We Need Assurance! (Brian Snow)</b></li>
     <li>Allen Dulles's 73 Rules of Spycraft (James Srodes)</li>
     <li>What the Tortoise Said to Achilles (Lewis Carroll)</li>
