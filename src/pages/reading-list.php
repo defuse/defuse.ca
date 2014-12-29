ad9e33815e4c0363b0657117d455ad08beb0bac0
Taylor Hornby <havoc@defuse.ca>
2014-12-29 13:38:30 -0700

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -138,6 +138,7 @@ search of the title is usually enough to find the book or paper.
 <h3>2014</h3>
 
 <ul>
+    <li>Acoustic Side-Channel Attacks on Printers (Michael Backes et al.)</li>
     <li><b>What is Teaching? (Paul R. Halmos)</b></li>
     <li>Beware of BGP Attacks (Ola Nordstr&ouml;m and Constantinos Dovrolis)</li>
     <li>A Pixel Is Not A Little Square (Alvy Ray Smith)</li>
