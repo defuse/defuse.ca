b6435a41348025017a3a39000d0258a6f17d5c1c
Taylor Hornby <havoc@defuse.ca>
2015-03-12 19:01:28 -0600

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -144,6 +144,7 @@ search of the title is usually enough to find the book or paper.
 <h3>2015</h3>
 
 <ul>
+    <li>Attacking The Linux PRNG on Android (David Kaplan et al.)</li>
     <li><b>Security Implications of Memory Deduplication in a Virtualized Environment (Jidong Xiao et al.)</b></li>
     <li>An Exploration of L2 Cache Covert Channels in Virtualized Environments (Yunjing Xu et al.)</li>
     <li>Everything You've Always Wanted To Know About Certificate Validation With OpenSSL (But Were Afraid To Ask) (Alban Diquet)</li>
