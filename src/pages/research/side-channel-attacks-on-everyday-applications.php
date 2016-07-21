13913699aa9ad7544c014d4b212ce325dfee6e83
Taylor Hornby <taylor@defuse.ca>
2016-07-21 17:12:14 -0600

    Remove outdated paragraph.

--- src/pages/research/side-channel-attacks-on-everyday-applications.php
+++ src/pages/research/side-channel-attacks-on-everyday-applications.php
@@ -1,10 +1,3 @@
-<h1>Compromising privacy with the FLUSH+RELOAD attack</h1>
+<h1>Side-Channel Attacks on Everyday Applications</h1>
 
-<p>
-Hello! You're probably visiting this page because you wanted the data and code
-from my paper on applying FLUSH+RELOAD to non-cryptographic applications. The
-code and data aren't published yet, as we're planning to submit this paper to
-WOOT15. I promise that as soon as the paper is published, the source code and
-data will be available here! If the paper doesn't get published, all of that
-will eventually show up here anyway.
-</p>
+<p>Coming soon!</p>
