d0ded734ba2f28888a21cba2eb156bfef12057a4
Taylor Hornby <taylor@defuse.ca>
2016-08-03 12:55:12 -0700

    Add links to side channel page

--- src/pages/research/side-channel-attacks-on-everyday-applications.php
+++ src/pages/research/side-channel-attacks-on-everyday-applications.php
@@ -1,3 +1,10 @@
 <h1>Side-Channel Attacks on Everyday Applications</h1>
 
-<p>Coming soon!</p>
+<p>This page will be updated with more information soon. Here are some
+links:</p>
+
+<ul>
+    <li><a href="/downloads/bh2016-whitepaper.pdf">Whitepaper</a></li>
+    <li><a href="https://github.com/defuse/flush-reload-attacks">Tools,
+Experiments, Source code, etc. on GitHub</a></li>
+</ul>
