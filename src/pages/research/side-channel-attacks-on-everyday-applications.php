ffeacb56b618df6d1e11ee1f623d1a0401e422a9
Taylor Hornby <taylor@defuse.ca>
2016-08-04 09:52:35 -0700

    Link to Black Hat slides.

--- src/pages/research/side-channel-attacks-on-everyday-applications.php
+++ src/pages/research/side-channel-attacks-on-everyday-applications.php
@@ -1,10 +1,12 @@
 <h1>Side-Channel Attacks on Everyday Applications</h1>
 
-<p>This page will be updated with more information soon. Here are some
-links:</p>
+<p>At Black Hat USA 2016, I presented my research on using cache side-channel
+attacks to attack everyday applications like web browsers and PDF viewers. You
+can find links to everything below.</p>
 
 <ul>
-    <li><a href="/downloads/bh2016-whitepaper.pdf">Whitepaper</a></li>
-    <li><a href="https://github.com/defuse/flush-reload-attacks">Tools,
-Experiments, Source code, etc. on GitHub</a></li>
+    <li>Talk Recording (Coming Soon).</li>
+    <li><a href="/downloads/us-16-Hornby-Side-Channel-Attacks-On-Everyday-Applications.pdf">Talk Slides</a></li>
+    <li><a href="/downloads/bh2016-whitepaper.pdf">Detailed Research Paper</a></li>
+    <li><a href="https://github.com/defuse/flush-reload-attacks">Tools, Experiments, and Source Code on GitHub</a></li>
 </ul>
