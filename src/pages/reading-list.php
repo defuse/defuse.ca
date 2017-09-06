b970a734ba05e0b308c8c8e68d21d2a4d7aba35b
Taylor Hornby <taylor@defuse.ca>
2017-09-06 19:42:20 -0400

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -191,6 +191,7 @@ to read. My favorites are highlighted in <b>bold text</b>.
 <h3>2017</h3>
 
 <ul>
+    <li><b>Why Do Developers Get Password Storage Wrong? A Qualitative Usability Study</b></li>
     <li><b>Motivated Numeracy and Enlightened Self-Government (Dan M Kahan et al.)</b></li>
     <li><b>When the Weakest Link is Strong: Secure Collaboration in the Case of the Panama Papers (Susan E. McGregor et al.)</b></li>
     <li>Open Problems in the Philosophy of Information (Luciano Floridi)</li>
