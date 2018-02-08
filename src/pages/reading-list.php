a61d518e6119d05bb985569ed58b3a2582c79bbd
Taylor Hornby <taylor@defuse.ca>
2018-02-08 14:46:07 -0500

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -19,6 +19,7 @@ to read. My favorites are highlighted in <b>bold text</b>.
 <h2>2018</h2>
 
 <ul>
+    <li>The Fabric of Reality (David Deutsch)</li>
     <li>Astrophysics for People in a Hurry (Neil deGrasse Tyson)</li>
 </ul>
 
