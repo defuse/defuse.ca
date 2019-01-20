afb5aaba707847eced79baf2a0f9f1257f0f4a45
Taylor Hornby <taylor@defuse.ca>
2019-01-20 13:39:47 -0700

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -205,6 +205,7 @@ to read. My favorites are highlighted in <b>bold text</b>.
 <h3>2018</h3>
 
 <ul>
+    <li>Symbolic Execution of Security Protocol Implementations: Handling Cryptographic Primitives (Mathy Vanhoef and Frank Piessens)</li>
     <li><b>An Empirical Analysis of Anonymity in Zcash (George Kappos et al.)</b></li>
     <li><b>Why Buy When You Can Rent? Bribery Attacks on Bitcoin-Style Consensus (Joseph Bonneau)</b></li>
     <li><b>Conciousness Engineered (Michael S.A. Graziano)</b></li>
