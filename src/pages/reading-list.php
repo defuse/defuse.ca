cd9b99698730047836ac264056fc00aad4766d08
Taylor Hornby <taylor@defuse.ca>
2020-09-14 14:02:41 -0600

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -239,6 +239,7 @@ to read. My favorites are highlighted in <b>bold text</b>.
 <h3>2020</h3>
 
 <ul>
+    <li>ConsenSGX: Scaling Anonymous Communications Networks with Trusted Execution Environments (Sajin Sasy and Ian Goldberg)</li>
     <li>k-Anonymity: A Model for Protecting Privacy (Latanya Sweeney)</li>
     <li>Anonymity, Unobservability, and Pseudonymity &ndash; A Proposal for Terminology (Andreas Pfitzmann and Marit K&ouml;hntopp)</li>
     <li><b>The Basics of Brain Development (Joan Stiles and Terry L. Jernigan)</b></li>
