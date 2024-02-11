518ffee2590668ed67b2e4e1fa0e6105941afee0
Taylor Hornby <taylor@defuse.ca>
2024-02-11 12:14:21 -0700

    update auditing examples

--- src/pages/services/software-security-auditing.php
+++ src/pages/services/software-security-auditing.php
@@ -5,7 +5,7 @@ I want to work with you to make sure your programs, your people, and your
 company are secure. I'm available for a daily rate to look over your protocol
 designs, software implementations, and for consulting to help you keep your
 business secure. To give back to the community, I substantially reduce my rate
-for published free or open source software (GPL, BSD, CC0, etc.) as well as
+for published open-source software (GPL, MIT, BSD, CC0, etc.) as well as
 open-access research.
 </p>
 
@@ -14,9 +14,9 @@ What I can do:
 </p>
 
 <ul>
-    <li>Evaluate the design and implementation of cryptography software.</li>
+    <li>Evaluate the design and implementation of cryptographic protocols.</li>
     <li>Check web code for XSS, SQL injection, session, password hashing, etc. vulnerabilities.</li>
-    <li>Find memory corruption bugs in C and C++ code.</li>
+    <li>Find memory corruption / threading bugs in C and C++ code.</li>
     <li>Write proof-of-concept exploits for vulnerabilities.</li>
     <li>Verifying vulnerability fixes.</li>
     <li>Train your developers and staff.</li>
@@ -27,6 +27,9 @@ Here are some examples of my security auditing work:
 </p>
 
 <ul>
+    <li><a href="https://zecsec.com/posts/zgo-security-audit/">Critical bugs found in ZGo, a cryptographic payment processor</a></li>
+    <li><a href="https://zecsec.com/posts/security-audit-of-hanhs-ledger-app/">Audit of a cryptocurrency hardware wallet</a></li>
+    <li><a href="https://github.com/defuse/ictm">The "invariant-centric" threat modeling approach</a></li>
     <li><a href="/audits/gocryptfs.htm">gocryptfs</a></li>
     <li><a href="/audits/encfs.htm">EncFS</a></li>
     <li><a href="/audits/ecryptfs.htm">eCryptfs</a></li>
