81c7cf91f3232ffe865ee77f81dc7c9d0271213c
Taylor Hornby <taylor@defuse.ca>
2024-11-15 16:00:18 -0700

    edits

--- src/pages/misc/fractal-zoom.php
+++ src/pages/misc/fractal-zoom.php
@@ -82,7 +82,7 @@ observed.
 </p>
 
 <p>
-Time fractures, folding itself into glistening polyhedra as you descend further.
+Time fractures, folding itself into glistening polyhedra as the descent continues.
 You feel yourself scattered across innumerable fractionally-dimensional levels
 of reality, your consciousness smeared over the edges, bound in uncountable
 mirrored fragments, each one experiencing a universe on the tip of a spiral.
