66e2b5458fac52c34d92a9002ec861a8156600bb
Taylor Hornby <taylor@defuse.ca>
2025-04-30 04:26:54 -0600

    RL

--- src/pages/reading-list.php
+++ src/pages/reading-list.php
@@ -289,6 +289,7 @@ to read. My favorites are highlighted in <b>bold text</b>.
 <h3>2025</h3>
 
 <ul>
+    <li><b>Safe curves for elliptic-curve cryptography (Daniel J. Bernstein and Tanja Lange)</b></li>
     <li>SoK: The Long Journey of Exploiting and Defending the Legacy of King Harald Bluetooth (Jianliang Wu et al.)</li>
 </ul>
 
